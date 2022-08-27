0d109edb31c7d2ea573482c0c3a94af3d8a0ba81
ClementINSA <clement.valence@gmail.com>
2022-08-27 18:42:15 +0200

    Create NeoPixelEffect.h

---
+++ source/NeoPixel/NeoPixelEffect.h
@@ -0,0 +1,29 @@
+/*
+ * NeoPixelEffect.h
+ *
+ *  Created on: Apr 11, 2020
+ *      Author: Clement Chaduc
+ */
+
+#ifndef NEOPIXELEFFECT_H_
+#define NEOPIXELEFFECT_H_
+
+#include <unistd.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdbool.h>
+
+#ifndef NUMBER_OF_LEDS
+#define NUMBER_OF_LEDS  30
+#endif
+
+void NeoPixelEffect_strobe(uint8_t r, uint8_t g, uint8_t b,
+                           uint16_t StrobeCount, uint16_t FlashDelay,
+                           uint16_t ledFirst, uint16_t ledCount);
+void NeoPixelEffect_fadeToBlack(uint16_t ledNo, uint8_t fadeValue);
+void NeoPixelEffect_meteorRain(uint8_t r, uint8_t g, uint8_t b,
+                               uint8_t meteorSize, uint8_t meteorTrailDecay, bool meteorRandomDecay, int SpeedDelay);
+void NeoPixelEffect_colorWheel(uint8_t WheelPos, uint8_t *r, uint8_t *g, uint8_t *b);
+
+#endif /* NEOPIXELEFFECT_H_ */
